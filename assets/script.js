173a7dd504aef8591372e47b2962a04be44832f3
erikchiodo <erikchiodo@gmail.com>
2023-02-26 11:00:28 -0500

    Added Timer. Updated HTML/CSS with New classes

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,93 @@
-const question = [
-  {
-    prompt: "Commonly used data types DO Not Include",
-    options: {
-      optionOne: "strings",
-      optionTwo: "booleans",
-      optionThree: "alerts",
-      optionFour: "numbers",
-    },
-    correctAnswer: "alerts",
-  },
-  {
-    prompt: "The condition in an if/else statment is enclosed with _______.",
-    options: {
-      optionOne: "quotes",
-      optionTwo: "curly brackets",
-      optionThree: "parenthesis",
-      optionFour: "square brackets",
-    },
-    correctAnswer: "parenthesis",
-  },
-  {
-    prompt: "Arrays in JavaScript can be used to store _______.",
-    options: {
-      optionOne: "numbers and strings",
-      optionTwo: "other arrays",
-      optionThree: "booleans",
-      optionFour: "all of the above",
-    },
-    correctAnswer: "all of the above",
-  },
-  {
-    prompt:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: {
-      optionOne: "JavaScript",
-      optionTwo: "terminal/bash",
-      optionThree: "for loops",
-      optionFour: "console.log",
-    },
-    correctAnswer: "console.log",
-  },
-];
+// Timer Function
+var timeEl = document.querySelector(".timer");
+var secondsLeft = 60;
 
-// let friendIndex = 0;
+function Timer() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Timer: " + secondsLeft;
 
-// function showFriend() {
-//   const currentFriend = myFriends[friendIndex];
+    if (secondsLeft == 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
 
-//   const friendHTML = `
-//     <div class="card" style="width: 18rem">
-//         <div class="card-body">
-//           <h5 class="card-title">${currentFriend.name}</h5>
-//           <ul>
-//             <li>${currentFriend.favorites.color}</li>
-//             <li>${currentFriend.favorites.number}</li>
-//             <li>${currentFriend.favorites.food}</li>
-//             </ul>
-//           <a href="#" class="btn btn-primary" onclick="nextFriend()">Next</a>
-//         </div>
+Timer();
+
+// const questions = [
+//   {
+//     prompt: "Commonly used data types DO Not Include",
+//     options: {
+//       optionOne: "strings",
+//       optionTwo: "booleans",
+//       optionThree: "alerts",
+//       optionFour: "numbers",
+//     },
+//     correctAnswer: "alerts",
+//   },
+//   {
+//     prompt: "The condition in an if/else statment is enclosed with _______.",
+//     options: {
+//       optionOne: "quotes",
+//       optionTwo: "curly brackets",
+//       optionThree: "parenthesis",
+//       optionFour: "square brackets",
+//     },
+//     correctAnswer: "parenthesis",
+//   },
+//   {
+//     prompt: "Arrays in JavaScript can be used to store _______.",
+//     options: {
+//       optionOne: "numbers and strings",
+//       optionTwo: "other arrays",
+//       optionThree: "booleans",
+//       optionFour: "all of the above",
+//     },
+//     correctAnswer: "all of the above",
+//   },
+//   {
+//     prompt:
+//       "A very useful tool used during development and debugging for printing content to the debugger is:",
+//     options: {
+//       optionOne: "JavaScript",
+//       optionTwo: "terminal/bash",
+//       optionThree: "for loops",
+//       optionFour: "console.log",
+//     },
+//     correctAnswer: "console.log",
+//   },
+// ];
+
+// let questionIndex = 0;
+
+// function showQuestions() {
+//   const currentQuestion = questions[questionIndex];
+
+//   const questionHTML = (
+//     <div class="card">
+//       <div class="card-body">
+//         <h5 class="card-title">${currentQuestion.prompt}</h5>
+//         <ul>
+//           <li>${currentQuestion.options.optionOne}</li>
+//           <li>${currentQuestion.options.optionTwo}</li>
+//           <li>${currentQuestion.options.optionThree}</li>
+//         </ul>
+//         <a href="#" class="btn btn-primary" onclick="nextQuestion()">
+//           Next
+//         </a>
 //       </div>
-//     `;
-
-//   $("#friend-container").html(friendHTML);
+//     </div>
+//   );
+//   $("#question-container").html(questionHTML);
 // }
 
-// function nextFriend() {
-//   friendIndex++;
+// function nextQuestion() {
+//   questionIndex++;
 
-//   if (friendIndex < myFriends.length) {
-//     showFriend();
+//   if (questionIndex < questions.length) {
+//     showQuestions();
 //   } else {
-//     $("#friend-container").html(`<h1>no MORE FRIENDS TO SHOW</h1>`);
+//     $("#question-container").html(`<h1>no MORE FRIENDS TO SHOW</h1>`);
 //   }
 // }
