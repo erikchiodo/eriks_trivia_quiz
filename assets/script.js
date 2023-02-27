0041fcb376592a0390975eca29168d42340202f2
erikchiodo <erikchiodo@gmail.com>
2023-02-26 22:13:36 -0500

    Created buttons for answer selections.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,84 @@
+// Quiz Questions
+const questions = [
+  {
+    prompt: "Q1. Commonly used data types DO Not Include",
+    options: {
+      optionOne: "strings",
+      optionTwo: "booleans",
+      optionThree: "alerts",
+      optionFour: "numbers",
+    },
+    correctAnswer: "alerts",
+  },
+  {
+    prompt:
+      "Q2. The condition in an if/else statment is enclosed with _______.",
+    options: {
+      optionOne: "quotes",
+      optionTwo: "curly brackets",
+      optionThree: "parenthesis",
+      optionFour: "square brackets",
+    },
+    correctAnswer: "parenthesis",
+  },
+  {
+    prompt: "Q3. Arrays in JavaScript can be used to store _______.",
+    options: {
+      optionOne: "numbers and strings",
+      optionTwo: "other arrays",
+      optionThree: "booleans",
+      optionFour: "all of the above",
+    },
+    correctAnswer: "all of the above",
+  },
+  {
+    prompt:
+      "Q4. A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: {
+      optionOne: "JavaScript",
+      optionTwo: "terminal/bash",
+      optionThree: "for loops",
+      optionFour: "console.log",
+    },
+    correctAnswer: "console.log",
+  },
+];
+
+let questionIndex = 0;
+
 // Displaying Questions
 function showQuestions() {
   const currentQuestion = questions[questionIndex];
 
-  const questionHTML = (
+  const questionHTML = `
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">${currentQuestion.prompt}</h5>
         <ul>
-          <li>${currentQuestion.options.optionOne}</li>
-          <li>${currentQuestion.options.optionTwo}</li>
-          <li>${currentQuestion.options.optionThree}</li>
+          <ol><button class = "question-btn">${currentQuestion.options.optionOne}</button></ol>
+          <ol><button class = "question-btn">${currentQuestion.options.optionTwo}</button></ol>
+          <ol><button class = "question-btn">${currentQuestion.options.optionThree}</button></ol>
+          <ol><button class = "question-btn">${currentQuestion.options.optionFour}</button></ol>
         </ul>
         <a href="#" class="btn btn-primary" onclick="nextQuestion()">
           Next
         </a>
       </div>
     </div>
-  );
+  `;
   $("#question-container").html(questionHTML);
 }
+
+// Function Call for Answer Selection (Correct/Incorrect)
+var questionBtn= $(".question-btn");
+
+function answerSelection() {
+  questionBtn.on("click", function () {
+    (if )
+
+
+})
+};
 
 // Looping through each question
 function nextQuestion() {
@@ -27,16 +87,9 @@ function nextQuestion() {
   if (questionIndex < questions.length) {
     showQuestions();
   } else {
-    $("#question-container").html(`<h1>no MORE FRIENDS TO SHOW</h1>`);
+    $("#question-container").html(`<h1>No More Questions</h1>`);
   }
 }
-
-// Set Up to Start Game (Initializing Game [Button] & On Click to Start)
-var StartButton = $("btn-primary");
-StartButton.addEventListener("click", function () {
-  showQuestions();
-  Timer();
-});
 
 // Timer Function
 var timeEl = document.querySelector(".timer");
@@ -53,49 +106,9 @@ function Timer() {
   }, 1000);
 }
 
-// Quiz Questions
-const questions = [
-  {
-    prompt: "Commonly used data types DO Not Include",
-    options: {
-      optionOne: "strings",
-      optionTwo: "booleans",
-      optionThree: "alerts",
-      optionFour: "numbers",
-    },
-    correctAnswer: "alerts",
-  },
-  {
-    prompt: "The condition in an if/else statment is enclosed with _______.",
-    options: {
-      optionOne: "quotes",
-      optionTwo: "curly brackets",
-      optionThree: "parenthesis",
-      optionFour: "square brackets",
-    },
-    correctAnswer: "parenthesis",
-  },
-  {
-    prompt: "Arrays in JavaScript can be used to store _______.",
-    options: {
-      optionOne: "numbers and strings",
-      optionTwo: "other arrays",
-      optionThree: "booleans",
-      optionFour: "all of the above",
-    },
-    correctAnswer: "all of the above",
-  },
-  {
-    prompt:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: {
-      optionOne: "JavaScript",
-      optionTwo: "terminal/bash",
-      optionThree: "for loops",
-      optionFour: "console.log",
-    },
-    correctAnswer: "console.log",
-  },
-];
-
-let questionIndex = 0;
+// Set Up to Start Game (Initializing Game [Button] & On Click to Start)
+var StartButton = $(".start-btn");
+StartButton.on("click", function () {
+  showQuestions();
+  Timer();
+});
